3445aae5eacd49d4ddadf057a7f28132b7beec71
wwmin <wwei.min@163.com>
2019-11-08 00:06:36 +0800

    add mq p1

---
+++ MQTest/Helper/MessageBody.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MQTest.Helper
+{
+    public class MessageBody
+    {
+        public EventingBasicConsumer Consumer { get; set; }
+        public BasicDeliverEventArgs BasicDeliver { get; set; }
+        /// <summary>
+        /// 0成功
+        /// </summary>
+        public int Code { get; set; }
+        public string Content { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool Error { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
