4683c93d2352e88f96876e1335502c84ae465d08
wwmin <wwei.min@163.com>
2019-11-14 17:14:06 +0800

    test EasyNetQ

---
+++ Client/WebClient/DTO/TextMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    public class TextMessage
+    {
+        public string Text { get; set; }
+    }
+}
--- Client/WebClient/MiddleWare/RequestMiddleware.cs
+++ Client/WebClient/MiddleWare/RequestMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EasyNetQ;
+using Messages;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,19 +8,26 @@ using System.Threading.Tasks;
 
 namespace WebClient.MiddleWare
 {
-    public class RequestMiddleware 
+    public class RequestMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly IBus _bus;
         public RequestMiddleware(RequestDelegate next)
         {
             _next = next;
+            _bus = RabbitHutch.CreateBus("host=localhost");
         }
 
         public async Task Invoke(HttpContext context)
         {
             var value = context.Request.Query["value"].ToString();
-            if(int.TryParse(value,out var number))
+
+            if (int.TryParse(value, out var number))
             {
+                _bus.Publish(new TextMessage
+                {
+                    Text = value
+                }); ;
                 await context.Response.WriteAsync($"the number is {number}");
             }
             else
