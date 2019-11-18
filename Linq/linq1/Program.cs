6ba147af8f98092bb3d31bbfb7aaa6a7c33ac7ae
wwmin <wwei.min@163.com>
2019-11-18 23:10:02 +0800

    feat: linq1

---
+++ Linq/linq1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Select1();
+        }
+
+        static void Select1()
+        {
+            Console.WriteLine("linq : select");
+            //使用集合初始化器给集合赋值
+            List<Employee> emp = new List<Employee>
+            {
+               new Employee(){Id=Guid.NewGuid(),Name="张三",Sex=0,CompanyName="xx技术有限公司"},
+               new Employee(){Id=Guid.NewGuid(),Name="李四",Sex=0,CompanyName="xx培训"},
+               new Employee(){Id=Guid.NewGuid(),Name="王五",Sex=0,CompanyName="xx集团"}
+            };
+
+            //查询表达式: 不能省略最后的select
+            var query = (from p in emp where p.Name.StartsWith("王") select p).FirstOrDefault();
+
+            //查询方法: 涉及到Lambda表达式,全部返回,可以省略最后的select , 延迟加载
+            var query1 = emp.Where(p => p.Name.StartsWith("王")).Select(e => new { e.Name, e.CompanyName });
+
+            //查询方法: 返回匿名类
+            var query2 = emp.Where(p => p.Name.StartsWith("王")).Select(p => p);
+            foreach (var item in query1)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+
+
+
+    class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Sex { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
