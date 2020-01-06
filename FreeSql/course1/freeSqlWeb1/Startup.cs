5091a17b0409e0993997c9fb00db6d359ab492b7
wwmin <wwei.min@163.com>
2020-01-06 17:18:31 +0800

    feat: 一些配置

--- FreeSql/course1/freeSqlWeb1/Startup.cs
+++ FreeSql/course1/freeSqlWeb1/Startup.cs
@@ -2,6 +2,8 @@ using FreeSql;
 using freeSqlWeb1.Configures;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,32 +13,35 @@ namespace freeSqlWeb1
 {
     public class Startup
     {
-
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
-
+            _env = env;
             Configuration = configuration;
             Fsql = new FreeSqlBuilder()
                 .UseConnectionString(DataType.MySql, Configuration.GetConnectionString("DatabaseMySql"))
                 .UseAutoSyncStructure(true)
                 .Build();
-            var loggerFactory = LoggerFactory.Create(builder =>
+            if (_env.IsDevelopment())
             {
-                builder
-                    .AddFilter("Microsoft", LogLevel.Warning)
-                    .AddFilter("System", LogLevel.Warning)
-                    .AddConsole()
-                    .AddEventLog();
-            });
-            ILogger logger = loggerFactory.CreateLogger<Program>();
-            Fsql.Aop.CurdBefore = (s, e) =>
-            {
-                logger.LogInformation(e.Sql);
-            };
-            Fsql.Aop.CurdAfter = (s, e) =>
-            {
-                //logger.LogInformation(s.ToString());
-            };
+                var loggerFactory = LoggerFactory.Create(builder =>
+                {
+                    builder
+                        .AddFilter("Microsoft", LogLevel.Warning)
+                        .AddFilter("System", LogLevel.Warning)
+                        .AddConsole()
+                        .AddEventLog();
+                });
+                ILogger logger = loggerFactory.CreateLogger<Program>();
+                Fsql.Aop.CurdBefore = (s, e) =>
+                {
+                    logger.LogInformation(e.Sql);
+                };
+                Fsql.Aop.CurdAfter = (s, e) =>
+                {
+                    //logger.LogInformation(s.ToString());
+                };
+            }
         }
 
 
@@ -47,11 +52,23 @@ namespace freeSqlWeb1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Some Configure
+            services.Configure<FormOptions>(options =>
+            {
+                options.MultipartBodyLengthLimit = long.MaxValue;//重置文件上传的大小
+                options.ValueLengthLimit = int.MaxValue;
+                //options.MultipartHeadersLengthLimit = int.MaxValue;
+            });
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = false; //=false为默认自动处理ModelState
+            });
+            #endregion
             #region use swagger
             services.UseSwagger();
             #endregion
             services.AddSingleton<IFreeSql>(Fsql);
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => options.UseCamelCasing(true)).AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new DateTimeJsonConverter()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
