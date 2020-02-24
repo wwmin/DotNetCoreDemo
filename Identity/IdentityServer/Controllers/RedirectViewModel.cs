105005ce24945e1504be584470074dc4ab359990
wwmin <wwei.min@163.com>
2020-02-24 18:03:16 +0800

    feat: IdentityJavaScriptClient 登录成功

---
+++ Identity/IdentityServer/Controllers/RedirectViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Controllers
+{
+    public class RedirectViewModel
+    {
+        public string RedirectUrl { get; set; }
+    }
+}
