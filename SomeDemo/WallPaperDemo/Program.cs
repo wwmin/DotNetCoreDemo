be03171867073130314db26e1b97fd7b1ab929eb
wwmin <wwei.min@163.com>
2020-04-15 14:09:37 +0800

    feat: 限定form窗口不能重复打开

--- SomeDemo/WallPaperDemo/Program.cs
+++ SomeDemo/WallPaperDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,6 +10,11 @@ namespace WallPaperDemo
     static class Program
     {
         /// <summary>
+        /// mutext互斥锁
+        /// </summary>
+        private static Mutex mutext = null;
+
+        /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
@@ -16,7 +22,32 @@ namespace WallPaperDemo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            GlobalMutext();
             Application.Run(new Form1());
+        }
+
+        private static void GlobalMutext()
+        {
+            bool createdNew = false;
+            string name = "WinformOneApp";
+            try
+            {
+                mutext = new Mutex(initiallyOwned: false, name, out createdNew);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                Thread.Sleep(1000);
+                Environment.Exit(1);
+            }
+            if (createdNew)
+            {
+                Console.WriteLine("程序已启动");
+                return;
+            }
+            MessageBox.Show("另一个窗口已在运行,不能重复运行.");
+            Thread.Sleep(1000);
+            Environment.Exit(1);
         }
     }
 }
