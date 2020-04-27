9d6ce7beec9836479ea182a434d2a6efc5fa50ec
wwmin <wwei.min@163.com>
2020-04-28 00:38:15 +0800

    feat: 关于t4模板

---
+++ CSharp/T4Test/TestXML.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T4Test
+{
+ //   public class TestXML
+ //   {
+ //       Catalog catalog = new catalog(@"..\data2.xml");
+ //       foreach (Artist artist in catalog.Artist)
+	//{
+ //           Console.WriteLine(artist.name);
+ //           foreach (Song song in artist.Song)
+	//{
+ //           Console.WriteLine(" "+song.Text);
+	//}
+	//}
+ //   }
+}
---
+++ CSharp/T4Test/t2.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+partial class Catalog
+{
+    private XmlNode thisNode;
+    public Catalog(XmlNode node)
+    {
+        thisNode = node;
+    }
+
+    public IEnumerable<Artist> Artist
+    {
+        get
+        {
+            foreach (XmlNode node in thisNode.SelectNodes("artist"))
+                yield return new Artist(node);
+        }
+    }
+}
+
+partial class Artist
+{
+    private XmlNode thisNode;
+    public Artist(XmlNode node)
+    {
+        thisNode = node;
+    }
+
+    public IEnumerable<Song> Song
+    {
+        get
+        {
+            foreach (XmlNode node in thisNode.SelectNodes("song"))
+                yield return new Song(node);
+        }
+    }
+    public string id { get { return thisNode.Attributes["id"].Value; } }
+    public string name { get { return thisNode.Attributes["name"].Value; } }
+}
+
+partial class Song
+{
+    private XmlNode thisNode;
+    public Song(XmlNode node)
+    {
+        thisNode = node;
+    }
+
+    public string Text { get { return thisNode.InnerText; } }
+    public string id { get { return thisNode.Attributes["id"].Value; } }
+}
+partial class Catalog
+{
+    public Catalog(string fileName)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(fileName);
+        thisNode = doc.SelectSingleNode("catalog");
+    }
+}
