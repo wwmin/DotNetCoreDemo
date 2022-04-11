a1a2d0821c5979eded1c94dc04ecb5c0cdd3a041
wwmin <wwei.min@163.com>
2022-04-11 11:40:59 +0800

    feat: file watcher

---
+++ Files/FileWatcher/FileUtil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcher;
+internal static class FileUtil
+{
+    private static FileSystemWatcher? s_watcher;
+
+
+    /// <summary>
+    /// file change watcher
+    /// </summary>
+    /// <param name="path">The directory to monitor, in standard or Universal Naming Convention (UNC) notation</param>
+    /// <param name="filter">The type of files to watch. For example, "*.txt" watches for changes to all text files.</param>
+    public static void WatchFiles(string path, string filter)
+    {
+        if (!Directory.Exists(path))
+        {
+            throw new FileNotFoundException();
+        }
+        s_watcher = new FileSystemWatcher(path, filter)
+        {
+            IncludeSubdirectories = true
+        };
+        s_watcher.Created += OnFileChanged;
+        s_watcher.Changed += OnFileChanged;
+        s_watcher.Deleted += OnFileChanged;
+        s_watcher.Renamed += OnFileRenamed;
+        s_watcher.EnableRaisingEvents = true;
+        Console.WriteLine("watching file changes ...");
+    }
+
+    public static void OnFileChanged(object sender, FileSystemEventArgs e)
+    {
+        Console.WriteLine($"file {e.Name} {e.ChangeType}");
+    }
+
+    public static void OnFileRenamed(object sender, RenamedEventArgs e)
+    {
+        Console.WriteLine($"file {e.OldName} {e.ChangeType} {e.Name}");
+    }
+}
+
---
+++ Files/FileWatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using FileWatcher;
+
+using System.Diagnostics;
+//var basePath = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());
+//var basePath = Environment.CurrentDirectory;
+//var basePath = AppDomain.CurrentDomain.BaseDirectory;
+//var basePath = Process.GetCurrentProcess()?.MainModule?.FileName;
+var basePath = $@"D:/";
+Console.WriteLine(basePath);
+FileUtil.WatchFiles(basePath, "*.txt");
+Console.Read();
